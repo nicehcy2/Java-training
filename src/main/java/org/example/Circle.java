080747bceb100e189b039c4873fc6a90cfa0d6e6
nicehcy <nicehcy2@naver.com>
2024-07-27 21:03:52 +0900

    접근자, 생성자 연습

--- src/main/java/org/example/Circle.java
+++ src/main/java/org/example/Circle.java
@@ -5,7 +5,31 @@ public class Circle {
     private int line;
     private int color;
 
+    // @NoArgsConstructor
+    public Circle() {
+    }
+
+    // @AllArgsConstructor
+    public Circle(double radius, int line, int color) {
+        this.radius = radius;
+        this.line = line;
+        this.color = color;
+    }
+
     public double findArea() {
         return 3.14 * radius * radius;
+    }
+
+    // @getter
+    public double getRadius() {
+        return radius;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public int getColor() {
+        return color;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-       Circle circle = new Circle();
+        Circle circle = new Circle(10, 1, 1);
 
         System.out.println(circle.findArea());
     }
