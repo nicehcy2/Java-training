210c771013833e8f9d7aaecd0d356564f27ec89d
nicehcy <nicehcy2@naver.com>
2024-07-27 20:54:53 +0900

    class, instace, object 연습

---
+++ src/main/java/org/example/Circle.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Circle {
+    private double radius;
+    private int line;
+    private int color;
+
+    public double findArea() {
+        return 3.14 * radius * radius;
+    }
+}
